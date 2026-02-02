99b22cc42dbc8c64140562472bc70860639567dc
Amir Maghami <Maghami.A1987@gmail.com>
2026-02-02 18:26:57 +0330

    update

--- server/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  isConnected(): boolean {
-    return true;
-  }
-}
--- server/src/auth/dto/auth.dto.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-import { ForgotSchema, LoginSchema, RegisterSchema } from '../schema/auth.schema';
-
-export type LoginDto = z.infer<typeof LoginSchema>;
-export type RegisterDto = z.infer<typeof RegisterSchema>;
-export type ForgotDto = z.infer<typeof ForgotSchema>;
