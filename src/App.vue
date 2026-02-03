b6aec1cab9e56658e32bd800974c069b74765ed9
Amir Maghami <Maghami.A1987@gmail.com>
2026-02-03 12:19:27 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <ion-app>
-    <Logo />
-    <!-- <ion-router-outlet /> -->
+    <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from "@ionic/vue";
-import Logo from "./components/Logo.vue";
 </script>
